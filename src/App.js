0fe2ac55e28e9905162c3ab8f80c10b05f565d48
farhanfazrian <farhanfazrian@gmail.com>
2021-10-28 20:01:20 +0700

    add route

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import './App.css';
 import { Route, Switch } from 'react-router-dom'
 
 import Movies from './containers/MoviesPage';
+import MovieDetailPage from './containers/MovieDetailPage';
 
 function App() {
   return (
     <Switch>
       <Route path="/movies" component={Movies} />
+      <Route path="/movie/:movieId" component={MovieDetailPage} />
     </Switch>
   );
 }
